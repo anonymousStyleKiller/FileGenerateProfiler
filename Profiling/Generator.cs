49c9a57190c083479eb61b18c92a1ce26317ec53
Anton <gumb1t97@gmail.com>
2022-08-15 23:36:56 +0300

    Generate file

---
+++ Profiling/Generator.cs
@@ -0,0 +1,34 @@
+﻿namespace Profiling;
+
+public sealed class Generator
+{
+    private readonly Random _random = new();
+    private string[] _words;
+    public Generator()
+    {
+
+        _words = Enumerable.Range(0, 1000)
+            .Select(_ =>
+            {
+                var range = Enumerable.Range(0, _random.Next(20, 100));
+                var chars = range.Select(x => (char)_random.Next('A', 'Z')).ToArray();
+                return new string(chars);
+            }).ToArray();
+    }
+
+    public void Generate(int linesCount)
+    {
+       var fileName = "L" + linesCount + ".txt";
+       using (var writer = new StreamWriter(fileName))
+       {
+           for (int i = 0; i < linesCount; i++)
+           {
+               writer.WriteLine(GenerateNumber()+ "."+ GenerateString());
+           }
+       }
+    }
+
+    private string GenerateString() => _words[_random.Next(0, _words.Length)];
+
+    private string GenerateNumber() => _random.Next(0, 10000).ToString();
+}
